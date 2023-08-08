a45781b2e0644715d6220a437a25d04b8ddb83cb
brian <brian.kimuya@student.moringaschool.com>
2023-08-08 10:13:21 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Transaction from './pages/Transaction';
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div>
+      <Transaction/>
+    </div>
+  );
+}
+
+export default App;
